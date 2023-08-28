eb634a3f771c3e094d43985aaa6e8ecdef2de94a
URIAN VIERA <urian1213viera@gmail.com>
2023-08-28 13:10:14 -0500

    Update views.py

--- MyAPI/views.py
+++ MyAPI/views.py
@@ -3,6 +3,26 @@ from django.shortcuts import render
 import requests
 
 
+
+def index(request):
+    productos = obtener_productos()
+    print(productos)
+    return HttpResponse("Hola Mundo")
+
+def obtener_productos():
+    # URL de productos
+    URL_API = "https://fakestoreapi.com/products"
+
+    # Realizar la solicitud GET a la API
+    response = requests.get(URL_API)
+
+    if response.status_code == 200:
+        productos = response.json()
+        return productos
+    return []
+
+
+
 def obtener_productos(request):
     # URL de productos
     URL_API = "https://fakestoreapi.com/products"
@@ -21,3 +41,4 @@ def obtener_productos(request):
         productos = []
 
     return render(request, 'index.html', {'productos': productos})
+    
