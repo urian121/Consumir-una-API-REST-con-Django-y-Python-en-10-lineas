cc1b9138dc52914c6da60f84da1f4a77bd366d01
URIAN VIERA <urian1213viera@gmail.com>
2023-12-02 21:11:09 -0500

    ajustando detalles

--- MyAPI/urls.py
+++ MyAPI/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-# Importando desde views.py la funcion obtener_productos
-from .views import obtener_productos
+# Importando desde views.py la funcion obtener_productos()
+from .views import getAPI, obtener_productos
 
 urlpatterns = [
     path('', obtener_productos, name='obtener_productos'),
+    path('api/', getAPI, name='getAPI'),
 ]
--- MyAPI/views.py
+++ MyAPI/views.py
@@ -1,45 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 # Importando Libreria Requests para hacer solicitudes HTTP
 import requests
 
 
-
 def index(request):
-    productos = obtener_productos()
+    productos = obtener_productos(request)
     print(productos)
     return HttpResponse("Hola Mundo")
 
-def obtener_productos():
+
+def getAPI(request):
     # URL de productos
-    URL_API = "https://fakestoreapi.com/products"
+    URL_API = "https://jsonplaceholder.typicode.com/albums"
 
     # Realizar la solicitud GET a la API
     response = requests.get(URL_API)
 
     if response.status_code == 200:
         productos = response.json()
-        # return response.json()
-        return productos
-    return []
-
+        return HttpResponse(productos) or []
 
 
 def obtener_productos(request):
     # URL de productos
     URL_API = "https://fakestoreapi.com/products"
 
-    # Realizar la solicitud GET a la API
-    response = requests.get(URL_API)
-
-    if response.status_code == 200:
-        productos = response.json()
-        # response.json() convierte el contenido de una respuesta HTTP (generalmente en formato JSON)
-        """for producto in productos:
-            print(producto)
-        """
-    else:
-        # Lista vacia
+    try:
+        # Intenta realizar la solicitud GET a la API
+        response = requests.get(URL_API)
+        if response.status_code == 200:
+            productos = response.json()
+        else:
+            # En caso de un código de respuesta no exitoso, manejar de acuerdo a tus necesidades
+            print(
+                f"Error en la solicitud. Código de respuesta: {response.status_code}")
+            productos = []
+    except requests.RequestException as e:
+        # Manejar errores de solicitud, por ejemplo, problemas de red
+        print(f"Error en la solicitud: {e}")
         productos = []
 
     return render(request, 'index.html', {'productos': productos})
-    
